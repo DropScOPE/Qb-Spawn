5410e7419ce25cbfb8fa879c4300add49dc0e80b
-DroppxDevs- <66690340+DropScOPE@users.noreply.github.com>
2022-09-07 17:07:59 +0100

    Add files via upload

---
+++ qb-spawn/config.lua
@@ -0,0 +1,27 @@
+QB = {}
+
+QB.Spawns = {
+    ["legion"] = {
+        coords = vector4(195.17, -933.77, 29.7, 144.5),
+        location = "legion",
+        label = "Legion Square",
+    },
+
+    ["policedp"] = {
+        coords = vector4(428.23, -984.28, 29.76, 3.5),
+        location = "policedp",
+        label = "Police Department",
+    },
+
+    ["paleto"] = {
+        coords = vector4(80.35, 6424.12, 31.67, 45.5),
+        location = "paleto",
+        label = "Paleto Bay",
+    },
+
+    ["pillbox"] = {
+        coords = vector4(239.6, -562.88, 43.28, 237.55),
+        location = "pillbox",
+        label = "Pillbox",
+    },
+}
